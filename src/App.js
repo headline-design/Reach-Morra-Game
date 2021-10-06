ec39d7752a7266a67deb8651e27bc7607b9cfb14
headline-design <85418370+headline-design@users.noreply.github.com>
2021-10-06 01:07:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ Object.assign(myEnv, {
   ALGO_PORT: "4180",
   ALGO_INDEXER_SERVER: "https://algocloud.org",
   ALGO_INDEXER_PORT: "8980",
-  REACH_ISOLATED_NETWORK: "no",
-  REACH_DEBUG: "1",
+  REACH_ISOLATED_NETWORK: "yes",
+  REACH_DEBUG: "false",
 })
 reach.setProviderByEnv(myEnv);
 
